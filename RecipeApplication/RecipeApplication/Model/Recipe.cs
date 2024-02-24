ddc6bb364876c1e522b6904a0ca5d0c326af2167
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-02-24 20:55:52 +0100

    Partly finished constructors for Recipe class

--- RecipeApplication/RecipeApplication/Model/Recipe.cs
+++ RecipeApplication/RecipeApplication/Model/Recipe.cs
@@ -8,19 +8,41 @@ namespace RecipeApplication.Model
 {
     public class Recipe
     {
+        #region properties
+
+        // fully implemented properties
         private string name;
         private string instruction;
 
         public string Name
         {
             get { return name; }
-            set {  name = value; }
+            set { name = value; }
         }
-       
+
         public string Instruction
         {
             get { return instruction; }
             set { instruction = value; }
         }
+        #endregion
+
+        #region Constructors
+        public Recipe()
+        {
+
+        }
+
+        public Recipe(string name)
+        {
+            Name = name;
+        }
+
+        public Recipe(string name, string instruction)
+        {
+            Name = name;
+            Instruction = instruction;
+        }
+        #endregion
     }
 }
