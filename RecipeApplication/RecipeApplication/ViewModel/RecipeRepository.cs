0273b938ad94d353884e1497b3bf2b43cd6a9ccf
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-02-25 21:57:16 +0100

    Finished (with no confirmation from teachers) code based on Use case "Udvide opskriftsamling"

---
+++ RecipeApplication/RecipeApplication/ViewModel/RecipeRepository.cs
@@ -0,0 +1,20 @@
+﻿using RecipeApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApplication.ViewModel
+{
+    class RecipeRepository
+    {
+        private List<Recipe> recipes = new List<Recipe>();
+         
+        public void AddRecipe (Recipe recipe)
+        {
+            recipes.Add(recipe);
+        } 
+
+    }
+}
