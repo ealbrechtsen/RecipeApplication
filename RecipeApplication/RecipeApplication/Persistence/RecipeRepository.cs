0bef962db947cdddec80ebbb0acf2853bc5c32e3
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-05-18 17:12:13 +0200

    Fixed json file

--- RecipeApplication/RecipeApplication/Persistence/RecipeRepository.cs
+++ RecipeApplication/RecipeApplication/Persistence/RecipeRepository.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RecipeApplication.Model;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration.Json;
+
+
 
 namespace RecipeApplication.Persistence
 {
