70c3e96511507e256262edb53aa6d4a671ebcc90
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-05-30 19:03:08 +0200

    fixed bug, initialize component working now

--- RecipeApplication/RecipeApplication/Views/MainWindow.xaml.cs
+++ RecipeApplication/RecipeApplication/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RecipeApplication
+namespace RecipeApplication.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
