cb72dde93972f4c56236f447c759e6404fb85fa5
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-06-02 16:15:29 +0200

    Cant see ConnectionString. error

--- RecipeApplication/RecipeApplication/Persistence/RecipeRepository.cs
+++ RecipeApplication/RecipeApplication/Persistence/RecipeRepository.cs
@@ -8,6 +8,7 @@ using Microsoft.Data.SqlClient;
 using System.Data.SqlClient;
 using System.Data;
 using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
 
 
 
@@ -15,6 +16,19 @@ namespace RecipeApplication.Persistence
 {
     public class RecipeRepository
     {
+        public string ConnnectionString { get; set; }
+        public void Create(Recipe recipe)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+            }
+        }
+        RecipeRepository()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
+            string? ConnectionString = config.GetConnectionString("MyDBConnection");
+        }
     }
 }
